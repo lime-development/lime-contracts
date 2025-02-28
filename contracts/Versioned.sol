fb1dfe88fa111d49a557cb12a9f9e7992d6d428b
VorobevSA <sv@vorobevsa.com>
2025-02-28 12:29:41 -0300

    Add comment to Versioned

--- contracts/Versioned.sol
+++ contracts/Versioned.sol
@@ -4,7 +4,17 @@
 // Commercial use requires a separate agreement with the author.
 pragma solidity ^0.8.20;
 
+ * @title Versioned
+ * @dev Contract that provides a unique version identifier based on its deployed bytecode.
+ * @author Vorobev Sergei
+ */
 contract Versioned  {
+    /**
+     * @notice Returns a unique identifier (hash) of the contract's bytecode.
+     * @dev Uses inline assembly to retrieve the contract's deployed bytecode and compute its keccak256 hash.
+     * This allows tracking of different contract versions based on their actual code.
+     * @return bytes32 Keccak256 hash of the contract's bytecode.
+     */
     function version() public view returns (bytes32) {
         address _address = address(this);
         uint256 size;
