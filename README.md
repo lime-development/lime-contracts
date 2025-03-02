126bb897d734998a0d7fa5c156d6d58049ba5a1d
VorobevSA <sv@vorobevsa.com>
2025-03-02 12:00:58 -0300

    Update deploy and ingo

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ Noorim is a decentralized platform designed to enable anyone to launch their own
 
 The project is built on a factory pattern, where the main contract acts as a factory that creates custom meme tokens for users, making it easy for anyone to create and launch their own token.
 
+## Dev
+
+### Automated Cross-Network SmartContract Testing
+
+This project includes an automated testing setup for smart contracts, ensuring they are tested across all supported networks with every commit to the repository. Whenever a change is made to the smart contracts, tests are executed on multiple networks to guarantee that the updates work correctly across all environments. This process provides confidence that any contract modifications will perform as expected, regardless of the network.
+
 ## Build
 
 ### NVM
@@ -21,5 +27,6 @@ npm install npm --global # Upgrade npm to the latest version
 ```
 npx hardhat compile
 npx hardhat test
+NETWORK=your_network npx hardhat test
 npx hardhat run scripts/deploy.js --network your_network
 ```
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,48 @@
+const networks = {
+  haqq: { name: "haqq", factory: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4" },
+  sepolia: { name: "sepolia", factory: "0x0227628f3F023bb0B980b67D528571c95c6DaC1c" },
+  base: { name: "base", factory: "0x33128a8fC17869897dcE68Ed026d694621f6FDfD" },
+  ethereum: { name: "ethereum", factory: "0x1F98431c8aD98523631AE4a59f267346ea31F984" },
+  bnb: { name: "bnb", factory: "0xdB1d10011AD0Ff90774D0C6Bb92e5C5c8b4461F7" }
+};
+
 async function main() {
-  const ContractMEME = await ethers.getContractFactory("ERC20Meme");
+  const ContractMEME = await ethers.getContractFactory("ERC20MEME");
   const ContractFactory = await ethers.getContractFactory("MemeFactory");
-      
+  const LiquidityFactory = await ethers.getContractFactory("getLiquidityHelper");
+  
   const [initialOwner] = await ethers.getSigners();
   console.log("Deploying contracts with the account:", initialOwner.address);
-  const meme  = await ContractMEME.deploy();
-  await meme.waitForDeployment();
 
-  const instance = await ContractFactory.deploy(meme.getAddress());
+  if (!networks[hre.network.name]) {
+    throw new Error(`Unsupported or missing network. Please specify a valid network (e.g., 'ethereum', 'sepolia', 'haqq', etc.)`);
+  }
+
+  const config = networks[hre.network.name];
+
+  // Deploying LiquidityHelper contract
+  const liquidityHelper = await LiquidityFactory.deploy();
+  await liquidityHelper.waitForDeployment();
+  console.log(`LiquidityHelper deployed to: ${await liquidityHelper.getAddress()}`);
+
+  // Deploying ERC20MEME contract
+  const meme = await ContractMEME.deploy();
+  await meme.waitForDeployment();
+  console.log(`ERC20MEME deployed to: ${await meme.getAddress()}`);
+
+  // Deploying MemeFactory contract with the appropriate factory address based on network
+  console.log("Deploy factory with Meme:", await meme.getAddress(), 
+              " UniswapV3 Facrory:",config.factory,
+              " liquidityHelper:", await liquidityHelper.getAddress());
+  const instance = await upgrades.deployProxy(ContractFactory, [
+    await meme.getAddress(),
+    config.factory, // Используем адрес Uniswap v3 Factory для выбранной сети
+    await liquidityHelper.getAddress()
+  ]);
   await instance.waitForDeployment();
-      
-  console.log(`Contract memeFactory deployed to ${await instance.getAddress()}, implementation is ${await meme.getAddress()}`);
+  console.log(`MemeFactory deployed to: ${await instance.getAddress()}`);
 }
-      
+
 main().catch((error) => {
   console.error(error);
 });
